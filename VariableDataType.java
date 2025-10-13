67e5372d64c1721578dd22ad5cd0d083d03e8d2e
nguyenhoangtuananh43-cyber <nguyenhoangtuananh43@gmail.com>
2025-10-13 14:33:25 +0700

    Add files via upload

---
+++ VariableDataType.java
@@ -0,0 +1,18 @@
+
+
+public class VariableDataType {
+   public static void main(String[] args) {
+int i= 10;
+float f = 20.5f;
+double D = 20.5;
+boolean b = true;
+char c = 'a';
+String s = "Hà Nội" ;
+System.out.println("i = " + i);
+System.out.println("f = " + f);
+System.out.println("D = " + D);
+System.out.println("b = " + b);
+System.out.println("s = " + s);
+System.out.println("c = " + c);
+    }
+}
