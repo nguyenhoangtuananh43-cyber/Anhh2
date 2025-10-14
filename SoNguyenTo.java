bd15b6cb0f9f4b37feb5168f108a41eb31296147
nguyenhoangtuananh43-cyber <nguyenhoangtuananh43@gmail.com>
2025-10-14 09:48:00 +0700

    Add files via upload

--- SoNguyenTo.java
+++ SoNguyenTo.java
@@ -1,30 +1,27 @@
 import java.util.Scanner;
 
-public class SoNguyenTo {
+class Prime {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a number: ");
+        System.out.print("Enter a number:");
         int number = scanner.nextInt();
-
-        if (number < 2) {
+        if (number < 2){
             System.out.println(number + " is not a prime");
         } else {
             int i = 2;
             boolean check = true;
-
-            while (i < number) {
+            while (i <= Math.sqrt(number)) {
                 if (number % i == 0) {
-                    check = false; // sửa Check → check
+                    check = false;
                     break;
                 }
                 i++;
             }
-
-            if (check) {
+            if (check)
                 System.out.println(number + " is a prime");
-            } else {
+            else
                 System.out.println(number + " is not a prime");
-            }
         }
     }
 }
